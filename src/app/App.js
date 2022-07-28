871b3c7880dd28012d6fd25683462102a311065a
SnackyTater <taterazay98@gmail.com>
2022-07-28 16:24:18 +0700

    update structure

---
+++ src/app/App.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from 'react'
+import { View, Text } from 'react-native'
+import SplashScreen from 'react-native-splash-screen'
+
+export function App() {
+
+    useEffect(() => {
+        SplashScreen.hide()
+    }, [])
+
+    return (
+        <View>
+            <Text>App</Text>
+        </View>
+    )
+}
